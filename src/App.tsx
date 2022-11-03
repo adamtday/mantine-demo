924e92b3b9fbff4ce853ba38d39ac5bc5c3175c4
adamtday <adamday3@gmail.com>
2022-11-03 14:38:29 -0400

    Update App.tsx
    import { MantineProvider, Text, Button, Stack } from "@mantine/core";
    import { theme } from "./theme";
    
    export default function App() {
      return (
        <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
          <Stack align="center" mt={50}>
            <Text size="xl" weight={500}>
              Welcome to Mantine!
            </Text>
            <Button>Click the button</Button>
          </Stack>
        </MantineProvider>
      );
    }

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { MantineProvider, Text, Button, Stack } from "@mantine/core";
-import { theme } from "./theme";
+import { MantineProvider, Text } from '@mantine/core';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
-      </Stack>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Text>Welcome to Mantine!</Text>
     </MantineProvider>
   );
 }
